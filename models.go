09ab19dcc1cea42e6b716a3034bcbc36eaead193
Xuerns <dmmaulana06@gmail.com>
2026-09-01 10:31:49 +0700

    update: test new workflow

--- models.go
+++ models.go
@@ -14,7 +14,7 @@ type User struct {
 	ApiKey    string    `json:"api_key"`
 }
 
-func databaseUserToUser(user database.User) (User, error) {
+func databaseUserToUser(user database.User) (User, error){
 	createdAt, err := time.Parse(time.RFC3339, user.CreatedAt)
 	if err != nil {
 		return User{}, err
