23a7dcd5683404c07fc189a518d41ebaab9df479
Xuerns <dmmaulana06@gmail.com>
2026-09-01 08:36:48 +0700

    update: test failure CI

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,17 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(test *testing.T) {
+	header := http.Header{}
+	header.Set("Authorization", "ApiKeyws cdsojncojsdnojcndsojcodsjkcsdkldjkvpkwkcznvojsncjfnnw0-31u29y8943932=2584935894389543-54893589347584389")
+
+	_, err := GetAPIKey(header)
+
+	if err != nil {
+		test.Errorf("Mayday maydat auo uo")
+	}
+}
